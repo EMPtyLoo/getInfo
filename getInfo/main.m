8cc99708ffd694e14f509c4f6254bcf5a555aa35
EMPtyLoo <457607792@qq.com>
2015-08-30 22:19:26 +0800

    Initial Commit

---
+++ getInfo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  getInfo
+//
+//  Created by EMPty on 8/30/15.
+//  Copyright (c) 2015 EMPty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
